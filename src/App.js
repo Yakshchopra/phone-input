70b69c3d1c2236b13c33e4cbff0f602116362a1a
Yaksh Chopra <yaksh@Yakshs-MacBook-Pro.local>
2023-07-17 19:20:01 +0530

    chore: poc for custom phone input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import 'react-phone-number-input/style.css';
+import PhoneInput from 'react-phone-number-input';
 import './App.css';
+import { useState } from 'react';
+import { isValidPhoneNumber } from 'react-phone-number-input';
+
+import {
+  getCountries,
+  getCountryCallingCode,
+} from 'react-phone-number-input/input';
+import en from 'react-phone-number-input/locale/en';
+
+const CountrySelect = ({ value, onChange, labels, ...rest }) => (
+  <select
+    {...rest}
+    value={value}
+    onChange={(event) => onChange(event.target.value || undefined)}
+  >
+    <option value=''>{labels['ZZ']}</option>
+    {getCountries().map((country) => (
+      <option key={country} value={country}>
+        {labels[country]} +{getCountryCallingCode(country)}
+      </option>
+    ))}
+  </select>
+);
 
 function App() {
+  const [value, setValue] = useState();
+
+  const handleCountryChange = (country) => {
+    const ele = document.querySelector('.PhoneInputCountry');
+    console.log(ele.innerHTML);
+
+    // ele.innerHTML += country;
+  };
+
+  const handleChange = (e) => {
+    console.log(e);
+    setValue(e);
+  };
+
+  const handleBlur = () => {
+    const isValid = isValidPhoneNumber(value);
+    if (!isValid) {
+      const ele = document.querySelector('.PhoneInput input');
+      ele.style.border = '1px solid red';
+    } else {
+      const ele = document.querySelector('.PhoneInput input');
+      ele.style.border = '1px solid var(--gray-g-6, #E2E2E2)';
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form autoComplete='off'>
+        <PhoneInput
+          placeholder='Enter phone number'
+          value={value}
+          onBlur={handleBlur}
+          defaultCountry='IN'
+          onCountryChange={handleCountryChange}
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
 }
